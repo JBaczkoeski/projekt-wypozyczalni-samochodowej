4b404603705a74295db251d769de21828be09fcc
Jakub Baczkowski <baczkowskikuba2@gmail.com>
2021-07-03 17:19:00 +0200

    początek

---
+++ navbar.php
@@ -0,0 +1,25 @@
+<?php
+	echo'
+	<div class="container-fluid">
+		<div class="row">		
+		<div class="col-sm-1 menu"><ul id="menu" class="mainMenu">
+			<div class="dropdown">
+				<button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Menu
+				</button>
+			  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				<a class="dropdown-item" href="#">Samochody i specyfika</a>
+				<a class="dropdown-item" href="#">Lokalizacje</a>
+				<a class="dropdown-item" href="#">Promocje</a>
+				<a class="dropdown-item" href="#">O Firmie</a>
+			  </div>
+			</div>
+		</div>
+		<div class="col-sm-1"><h4><a href="#"><button type="button" class="btn btn-secondary">Kontakt</button></a></h4></div>
+		<div class="col-sm-2"><h4><a href="wiadomość.php"><button type="button" class="btn btn-secondary">Wyślij Wiadomość</button></a></h4></div>
+		<div class="col-sm-2"><h4><a href="adduser.php"><button type="button" class="btn btn-secondary">Utwórz konto</button></a></h4></div>
+		<div class="col-sm-2"><h4><a href="logowanie.php"><button type="button" class="btn btn-secondary">Zaloguj się</button></a></h4></div>		
+		</div>
+	</div>
+	';
+?>
--- projekt.php
+++ projekt.php
@@ -1,9 +1,6 @@
 <?php
-
-
-
-
-
+include('bt.php');
+include('navbar.php');
 ?>
 <html>
 
@@ -14,33 +11,14 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="projekt.css">
+	<link rel="stylesheet" href="">
 	<script src="projekt.js"></script> 
 </head>
 
 <body>
 <div class="panel panel-default">
 	<div class="panel-heading" >
-		<h3><p class="text-end">Wypożyczalnia Samochodów</p></h3>
-	<div class="row">
-		<div class="col-sm-1 menu"><ul id="menu" class="mainMenu">
-			<div class="dropdown">
-				<button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Menu
-				</button>
-			  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-				<a class="dropdown-item" href="#">Samochody i specyfika</a>
-				<a class="dropdown-item" href="#">Lokalizacje</a>
-				<a class="dropdown-item" href="#">Promocje</a>
-				<a class="dropdown-item" href="#">O Firmie</a>
-			  </div>
-			</div>
-		</div>
-		<div class="col-sm-1 menu2"><h4><a href="#"><button type="button" class="btn btn-secondary">Kontakt</button></a></h4></div>
-		<div class="col-sm-2"><h4><a href="wiadomość.php"><button type="button" class="btn btn-secondary">Wyślij Wiadomość</button></a></h4></div>
-		<div class="col-sm-2 menu2"><h4><a href="adduser.php"><button type="button" class="btn btn-secondary">Utwórz konto</button></a></h4></div>
-		<div class="col-sm-2 menu2 menu3"><h4><a href="logowanie.php"><button type="button" class="btn btn-secondary">Zaloguj się</button></a></h4></div>
-	</div>
+		
 
 
 </div>
