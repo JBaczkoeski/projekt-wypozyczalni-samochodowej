8948b082fa4d85441131187020e28864cc5bd9eb
Jakub Baczkowski <baczkowskikuba2@gmail.com>
2021-07-25 12:18:43 +0200

    projekt
    początek stopka i navbar gotowe i bootstrap 5

--- navbar.php
+++ navbar.php
@@ -1,25 +1,42 @@
 <?php
 	echo'
-	<div class="container-fluid">
-		<div class="row">		
-		<div class="col-sm-1 menu"><ul id="menu" class="mainMenu">
-			<div class="dropdown">
-				<button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Menu
-				</button>
-			  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-				<a class="dropdown-item" href="#">Samochody i specyfika</a>
-				<a class="dropdown-item" href="#">Lokalizacje</a>
-				<a class="dropdown-item" href="#">Promocje</a>
-				<a class="dropdown-item" href="#">O Firmie</a>
-			  </div>
-			</div>
-		</div>
-		<div class="col-sm-1"><h4><a href="#"><button type="button" class="btn btn-secondary">Kontakt</button></a></h4></div>
-		<div class="col-sm-2"><h4><a href="wiadomość.php"><button type="button" class="btn btn-secondary">Wyślij Wiadomość</button></a></h4></div>
-		<div class="col-sm-2"><h4><a href="adduser.php"><button type="button" class="btn btn-secondary">Utwórz konto</button></a></h4></div>
-		<div class="col-sm-2"><h4><a href="logowanie.php"><button type="button" class="btn btn-secondary">Zaloguj się</button></a></h4></div>		
-		</div>
-	</div>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Twój wymarzony samochód</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Strona Główna <span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">Samochody</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Wyślij Wiadomość</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Utwórz konto</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">Zaloguj się</a>
+      </li>
+	  <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Menu
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Kontakt</a>
+          <a class="dropdown-item" href="#">Lokalizacje</a>
+		  <a class="dropdown-item" href="#">Współpraca</a>
+		  <a class="dropdown-item" href="#">O Firmie</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
 	';
 ?>
--- projekt.php
+++ projekt.php
@@ -7,34 +7,14 @@ include('navbar.php');
 <head>
 	<meta charset="utf-8">
 		<title>Twój wymarzony samochód</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="projekt.css">
 	<script src="projekt.js"></script> 
 </head>
 
 <body>
-<div class="panel panel-default">
-	<div class="panel-heading" >
 		
-
-
-</div>
-	<div class="panel-body">
-		
-		
-		
-		
-
-
-
-
-
-
-</div>
-</div>
 </body>
-
 </html>
+<?php
+include('footer.php');
+?>
