13c9ec6e10f9f4ff4e768c620f6583c48cbc64a7
JBaczkowski <81856244+JBaczkoeski@users.noreply.github.com>
2021-09-20 18:02:04 +0200

    20.09.2021
    zrobić o firmie i logowanie

--- navbar.php
+++ navbar.php
@@ -1,13 +1,15 @@
 	<?php
   echo'
   <nav class="navbar navbar-dark bg-dark sticky-top navbar-expand-lg">
+  <div class="container col-7">
   <a class="text-white h2" href="projekt.php">Twój wymarzony samochód</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
-  <div class="collapse navbar-collapse justify-content-end mr-5" id="navbarSupportedContent">
-	<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+</div>
+<div class="contaniner col-8 d-flex justify-content-end">
+  <div class="collapse navbar-collapse d-flex" id="navbarSupportedContent">
+	<ul class="navbar-nav mb-2 mb-lg-0 d-flex">
         <li class="nav-item">
           <a class="nav-link active h5" aria-current="page" href="projekt.php">Strona główna</a>
         </li>
@@ -27,6 +29,8 @@
           <a class="nav-link h5" href="#">Zarejestruj się</a>
         </li>
   </div>
+  </div>
 </nav>
+
 ';
 ?>
--- wiadomości.php
+++ wiadomości.php
@@ -10,46 +10,47 @@ include ('navbar.php');
 	        <link rel="stylesheet" href="projekt.css">
         </head>
         <body>
-            <div id="container-fluid">
-                <div class="row">
-                    <div class="container col-5 bg-gray text-white text-center ">
-                        <div class="row d-flex justify-content-center">
-                            <div class="col-12 text-center h2">
+                    <div class="container-fluid col-7 bg-gray text-white text-center h-100">
+                        <div class="row d-flex justify-content-center pb-5">
+                            <div class="col-12 text-center h2 mb-5 pt-5">
                                 Wyślij wiadomość
                             </div>
                             <form class="form col-8 text-center">
                                 <div class="row">
-                                    <div class="col-6">
+                                    <div class="col-6 mb-4">
                                         <input type="text" class="form-control" placeholder="Imię">
                                     </div>
-                                    <div class="col-6">
+                                    <div class="col-6 mb-4">
                                         <input type="text" class="form-control" placeholder="E-mail">
                                     </div>
-                                    <div class="col-6">
-                                        <select class="form-select form-select-lg" aria-label=".form-select-lg example">
-                                            <option selected>Open this select menu</option>
-                                            <option value="1">One</option>
-                                            <option value="2">Two</option>
-                                            <option value="3">Three</option>
+                                    <div class="col-6 mb-4">
+                                        <select class="form-select" aria-label=".form-select-lg example">
+                                            <option selected>Powód</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
                                         </select>
                                     </div>
-                                    <div class="col-6">
-                                        <input type="text" class="form-control" placeholder="Imię">
+                                    <div class="col-6 mb-4">
+                                        <input type="text" class="form-control" placeholder="Miasto">
                                     </div>
-                                    <div class="col-6">
-                                        <input type="text" class="form-control" placeholder="Imię">
+                                    <div class="col-12 mb-4">
+                                        <textarea type="text" class="form-control" placeholder="Tekst"></textarea>
                                     </div>
-                                    <div class="col-6">
-                                        <input type="text" class="form-control" placeholder="Imię">
+                                    <div class="col-12 mb-4">
+                                        <button type="submit" class="btn btn-success">Wyślij</button>
+                                    </div>
+                                    <div class="col-12 mb-4">
+                                        <button type="clear" class="btn btn-danger">Wyczyść</button>
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </div>
-                </div>
-            </div>
         </body>
     </html>
+<div class="fixed-bottom">
 <?php
 include ('footer.php');
 ?>
+</div>
